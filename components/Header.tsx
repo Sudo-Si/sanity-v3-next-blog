b97c717a45a3a2003f0c9a1eb6de5f454a227baa
Sudo-Si <dalgleishmaswabi@gmail.com>
2023-01-09 17:21:06 +1300

    Header styling adjust before the frontend gets loaded in next is preview mode

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,20 +7,20 @@ import {FaArrowRight} from "react-icons/fa"
 // TODO Adjust nav buttons 
 function Header() {
   return (
-    <header className="flex items-center justify-between space-x-2 font-bold px-10 py-1 my-0">
+    <header className="flex items-center justify-between space-x-2 font-bold px-0 py-1 my-0">
       <div className=" flex items-center space-x-2">
-        <Link href="/" className="flex items-center space-x-2">
+        <Link href="/" className="flex items-center space-x-2 px-4">
         <Image
         src="https://i.postimg.cc/Px3pxSHz/A-Sk-ll-Called-L-CK-2.png" 
         width={40}
         height={40}
         alt= "logo"
-        /> <h1>The Yarn 2.0</h1>
+        /> <h1>theYarn2.0</h1>
         </Link>
        
       </div>
 
-      <div className="flex items-center space-x-3  ">
+      <div className="flex items-center space-x-3 px-4 ">
        <h3> More of my stuff </h3><FaArrowRight className="animate-bounce w-6 "/>
         <Link className=" hover:bg-sky-600 " href={"https://github.com/Sudo-Si?tab=repositories"}><FaGithubSquare/> </Link>
         <Link className="  hover:bg-sky-600 " href={"https://www.linkedin.com/in/othusitse-dalgleish-maswabi/"}><FaLinkedin/></Link>
